19df4ada15475c3667c44211e1995a499639b4ef
Jorge Vera <me@jorgevera.net>
2020-03-31 23:23:04 -0400

    Add netlify-identity-widget

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+import { persistUser, logoutUser } from '../lib/identity'
 
-function Login() {
-  return (
-    <div>
-      <p>Login...</p>
-    </div>
-  )
+function LogIn() {
+  useEffect(() => {
+    netlifyIdentity.on('login', user => persistUser(user))
+    netlifyIdentity.on('logout', () => logoutUser())
+  })
+
+  const handleLogIn = () => netlifyIdentity.open()
+
+  return <button onClick={handleLogIn}>Log in</button>
 }
 
-export default Login
+export default LogIn
