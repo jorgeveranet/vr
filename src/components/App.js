21106847fe641aebdc2139b4845e3b02371eb75d
Jorge Vera <me@jorgevera.net>
2020-03-31 22:41:42 -0400

    Add components

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Login from './Login'
+import Dashboard from './Dashboard'
+
+function App() {
+  return (
+    <div>
+      <Dashboard></Dashboard>
+    </div>
+  )
+
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default App
